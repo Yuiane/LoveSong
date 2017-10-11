5bdaa706d5b1630c01f7c95a5cdc2b7183c1b0b4
yuiane <yuiane@LAPTOP-2VI8E9AQ>
2017-10-11 21:13:33 +0800

    提交app图标和启动图和闪屏轮播图

--- LoveSong.App/src/app/app.component.ts
+++ LoveSong.App/src/app/app.component.ts
@@ -1,22 +1,35 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
-
+import { welcomePage } from '../pages/welcome/welcome';
 import { TabsPage } from '../pages/tabs/tabs';
-
+import { Storage } from '@ionic/storage';
 
 @Component({
-  templateUrl: 'app.html'
+    templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = TabsPage;
+    rootPage: any;
+    constructor(platform: Platform, public storage: Storage) {
+        this.storage.clear();
+        this.storage.get('firstIn').then((result) => {
+            if (result) {
+                this.rootPage = TabsPage;
+            }
+            else {
+                this.storage.set('firstIn', true);
+                this.rootPage = welcomePage;
+            }
+        });
 
-  constructor(platform: Platform) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-      Splashscreen.hide();
-    });
-  }
+        platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            StatusBar.styleLightContent();
+            setTimeout(function () {
+                Splashscreen.hide();
+            }, 1000);
+        });
+
+    }
 }
---
+++ LoveSong.App/src/pages/welcome/welcome.ts
@@ -0,0 +1,25 @@
+﻿import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
+
+/*
+  Generated class for the welcome page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+    selector: 'page-welcome',
+    templateUrl: 'welcome.html'
+})
+export class welcomePage {
+
+    constructor(public navCtrl: NavController, public navParams: NavParams) { }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad welcomePage');
+    }
+    goToHome() {
+        this.navCtrl.setRoot(HomePage);
+    }
+}
